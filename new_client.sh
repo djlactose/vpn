c5e4b8dcc61fdf0f86af53f43d8372af772d1321
Nick Hernandez <nickhernandez@djlactose.com>
2019-06-27 14:40:53 -0400

    Change to new client script to make the tar work

--- new_client.sh
+++ new_client.sh
@@ -3,10 +3,10 @@ if [ ! "$BASH_VERSION" ] ; then
     exec /bin/bash "$0" "$@"
 fi
 cd /etc/openvpn
-/usr/share/easy-rsa/easyrsa build-server-full $1 nopass
+/usr/share/easy-rsa/easyrsa build-client-full $1 nopass
 #echo $1 | /usr/share/easy-rsa/easyrsa gen-req $1 nopass
 #echo yes | /usr/share/easy-rsa/easyrsa sign-req client $1
 echo Private Key:  /etc/openvpn/pki/private/$1.key
 echo Cert:  /etc/openvpn/pki/issued/$1.crt
 echo CA:   /etc/openvpn/pki/ca.cert
-tar -zcf /etc/openvpn/certPacks/$1.tgz -C /etc/openvpn/pki/private/$1.key -C /etc/openvpn/pki/issued/$1.crt -C /etc/openvpn/pki/ca.crt
+tar -zcf /etc/openvpn/certPacks/$1.tgz -C /etc/openvpn/pki/private/ $1.key -C /etc/openvpn/pki/issued/ $1.crt -C /etc/openvpn/pki/ ca.crt
