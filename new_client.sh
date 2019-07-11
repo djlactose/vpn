a8da89667d656e45bb0ec06708fbde516448a260
Nick Hernandez <nickhernandez@djlactose.com>
2019-07-11 11:28:44 -0400

    Added self contained ovpn file generation when creating a new client

--- new_client.sh
+++ new_client.sh
@@ -5,11 +5,11 @@ fi
 read -p "Please enter the device: " device
 cd /etc/openvpn
 /usr/share/easy-rsa/easyrsa build-client-full $device nopass
-#echo $device | /usr/share/easy-rsa/easyrsa gen-req $device nopass
-#echo yes | /usr/share/easy-rsa/easyrsa sign-req client $device
 echo Private Key:  /etc/openvpn/pki/private/$device.key
 echo Cert:  /etc/openvpn/pki/issued/$device.crt
 echo CA:   /etc/openvpn/pki/ca.cert
+
+#Create Cert Bundle
 cp /etc/openvpn/pki/private/$device.key /etc/openvpn/
 cp /etc/openvpn/pki/issued/$device.crt /etc/openvpn/
 cp /etc/openvpn/pki/ca.crt /etc/openvpn
@@ -17,3 +17,15 @@ tar -zcf /etc/openvpn/certPacks/$device.tgz -C /etc/openvpn/ $device.key $device
 rm /etc/openvpn/$device.key
 rm /etc/openvpn/$device.crt
 rm /etc/openvpn/ca.crt
+
+#Self Contained Config Generation
+cp /etc/openvpn/client.ovpn /etc/openvpn/certPacks/$device.ovpn
+echo "<key>" >> /etc/openvpn/certPacks/$device.ovpn
+echo "$(cat /etc/openvpn/pki/private/$device.key)" >> /etc/openvpn/certPacks/$device.ovpn
+echo "</key>" >> /etc/openvpn/certPacks/$device.ovpn
+echo "<cert>" >> /etc/openvpn/certPacks/$device.ovpn
+echo "$(cat /etc/openvpn/pki/issued/$device.crt)" >> /etc/openvpn/certPacks/$device.ovpn
+echo "</cert>" >> /etc/openvpn/certPacks/$device.ovpn
+echo "<ca>" >> /etc/openvpn/certPacks/$device.ovpn
+echo "$(cat /etc/openvpn/pki/ca.crt)" >> /etc/openvpn/certPacks/$device.ovpn
+echo "</ca>" >> /etc/openvpn/certPacks/$device.ovpn
